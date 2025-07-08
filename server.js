85330870c7d6dfcacf7355b45bd4a546c56456c5
Frances Reagan James <adtgwen@gmail.com>
2025-07-08 01:03:20 -0400

    added route handler for GET requests and app listen to start server

--- server.js
+++ server.js
@@ -15,9 +15,21 @@ const PORT = 3000;
 // this allows CSS, images, and other static files to be accessed directly//
 app.use(express.static("public"));
 
-// route handler for GET requests to the rool URL(/)//
+// route handler for GET requests to the root URL(/)//
 app.get("/", (req, res) => {
   // send the index.html file from the public directory//
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// route handler for GET requests to /contact//
+app.get("/contact", (req, res)=>{
+  // send the contact.html file from the public directory//
+  res.sendFile(path.join(__dirname,"public","contact.html"));
+});
+
+// start the server and listen on the specified port//
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Visit http://localhost:${PORT} to view the homepage`);
+  console.log(`Visit http://localhost:${PORT}/contact to view the contact page`);
+});
