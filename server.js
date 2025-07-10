5d9aab19cf70c331f1318c95bff6805cd2aae4ba
Frances Reagan James <adtgwen@gmail.com>
2025-07-09 23:32:45 -0400

    added route handlers for root url and /contact

--- server.js
+++ server.js
@@ -11,12 +11,29 @@ const app = express();
 // define the port number//
 const PORT = 3000;
 
-
+// serve static files from the public directory//
+// if I add --this --access CSS, images, and other static files directly//
 app.use(express.static("public"));
 
+
+//ROUTES//
+// route handler for GET requests to the root url(/)//
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname,"public","index.html"));
 });
+
+// route handler for GET requests to /contact/ //
+app.get("/contact", (req, res) => {
+  // send the contact.html file from the public directory//
+  res.sendFile(path.join(__dirname,"public","contact.html"));
+});
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
