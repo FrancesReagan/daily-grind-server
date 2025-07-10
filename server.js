182a67916336cc4ab23fddcc90c09863f5ef3afc
Frances Reagan James <adtgwen@gmail.com>
2025-07-09 23:27:24 -0400

    first commit for lab2 server to server refactor daily grind with axios

--- server.js
+++ server.js
@@ -1,35 +1,23 @@
-// ServiceWorker.js//
-// import express libary//
-const express = require("express");
-
+// mod 12 lesson 6 Lab2//
+//daily grind with axios -- fun facts api//
+//import express library//
+const express = require ("express");
+// import axios for API requests//
+const axios = require ("axios");
 // import built-in path module//
-const path = require("path");
-
+const path = require ("path");
 // create an instance of an express application//
 const app = express();
-
 // define the port number//
 const PORT = 3000;
 
-// serve static files from the public directory//
-// this allows CSS, images, and other static files to be accessed directly//
+
 app.use(express.static("public"));
 
-// route handler for GET requests to the root URL(/)//
 app.get("/", (req, res) => {
-  // send the index.html file from the public directory//
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname,"public","index.html"));
 });
 
-// route handler for GET requests to /contact//
-app.get("/contact", (req, res)=>{
-  // send the contact.html file from the public directory//
-  res.sendFile(path.join(__dirname,"public","contact.html"));
-});
-
-// start the server and listen on the specified port//
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(`Visit http://localhost:${PORT} to view the homepage`);
-  console.log(`Visit http://localhost:${PORT}/contact to view the contact page`);
-});
+})
